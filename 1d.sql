12a9f613149c21466dab6a912d9d1167a527fda8
bidevpadma <padmavathi.chiraj@gmail.com>
2021-08-08 14:12:42 +1000

    modified 1d

--- 1d.sql
+++ 1d.sql
@@ -1,3 +1,4 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT *
-  FROM [Keys].[dbo].[Role]
+SELECT j.Id, j.JobDescription,j.JobStartDate,j.JobEndDate, js.[Status]
+FROM [Keys].[dbo].[Job] j
+inner join JobStatus js on j.JobStatusId = js.Id
+where status not in ('Finished','Cancelled','Deleted');
