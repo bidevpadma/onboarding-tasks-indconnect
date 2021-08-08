8fb8ce2c1f18189a73480e1a8198d622fccd299e
bidevpadma <padmavathi.chiraj@gmail.com>
2021-08-08 16:24:40 +1000

    delete the files outside

--- 1a.sql
+++
@@ -1,6 +0,0 @@
-/****** Script for Display a list of all property names and their property id’s for Owner Id: 1426  ******/
-SELECT 
-[PropertyId],p.Name
-FROM [Keys].[dbo].[OwnerProperty] op
-inner join [Keys].[dbo].[Property] p on op.PropertyId = p.Id
-WHERE op.OwnerId = 1426;	
--- 1d.sql
+++
@@ -1,4 +0,0 @@
-SELECT j.Id, j.JobDescription,j.JobStartDate,j.JobEndDate, js.[Status]
-FROM [Keys].[dbo].[Job] j
-inner join JobStatus js on j.JobStatusId = js.Id
-where status not in ('Finished','Cancelled','Deleted');
