6d066bb20680af98043ab118ee977dcad88e9a8d
bidevpadma <padmavathi.chiraj@gmail.com>
2021-08-11 23:19:28 +1000

    modified 1ci

--- Task1/1ci.sql
+++ Task1/1ci.sql
@@ -1,12 +1,21 @@
 /****** Script for Display a list of all property names and their property id’s for Owner Id: 1426  and sum of all payments from start date to end date******/
-SELECT 
-op.OwnerId, op.[PropertyId],p.Name,trt.Name as 'RentalPaymentFrequency',SUM(prp.amount) as 'PaymentsSum'
+SELECT p.name,trt.name as 'RentalPaymentFrequency',tp.PaymentAmount,
+ (CASE
+ when tp.PaymentFrequencyId= 1 then DATEDIFF(WEEK,tp.StartDate,tp.EndDate)
+ when tp.PaymentFrequencyId = 2 then DATEDIFF(MONTH,tp.StartDate,tp.EndDate)/2 
+ when tp.PaymentFrequencyId = 3 then DATEDIFF(MONTH,tp.StartDate,tp.EndDate)
+ else Null
+ END)*PaymentAmount as 'Sum Of All Payments'
 FROM [Keys].[dbo].[OwnerProperty] op
 inner join [Keys].[dbo].[Property] p on op.PropertyId = p.Id
 inner join [Keys].[dbo].[PropertyRentalPayment] prp on prp.PropertyId = p.Id
 inner join [Keys].[dbo].[TargetRentType] trt on trt.Id = prp.FrequencyType 
 inner join [Keys].[dbo].[TenantProperty] tp on tp.PropertyId = p.Id 
 WHERE 
-op.OwnerId = 1426 and 
-prp.Date between tp.StartDate and tp.EndDate
-GROUP BY op.OwnerId,op.[PropertyId],p.Name,trt.Name
+op.OwnerId = 1426
+
+
+
+
+
+ 
